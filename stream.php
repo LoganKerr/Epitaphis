70738d2daeaf6b46344674f9afa2ccce54f5a816
tom mulvey <tommulvey5567@gmail.com>
2018-04-08 04:51:48 -0400

    stream php and html

---
+++ stream.php
@@ -0,0 +1,29 @@
+<?php
+    session_start();
+    ob_start();
+    header('Content-Type: text/html; charset=utf-8');
+    
+    require_once("config/config.php");
+    require_once("functions.php");
+    require_once("vendor/autoload.php");
+    
+    // if user is not signed in
+    if (!isset($_SESSION['user_id']))
+    {
+        header("Location: index.php");
+    }
+    
+    $user_id = $_SESSION['user_id'];
+
+    $loader = new Twig_Loader_Filesystem('resources/views');
+    $twig = new Twig_Environment($loader);
+    
+    $admin = check_if_user_is_admin($_SESSION['user_id']);
+    
+    echo $twig->render('stream.html', array(
+                                             'nav' => array('page' => $_SERVER['PHP_SELF'], 'admin' => $admin),
+                                             'rows' => $rows,
+                                             'bio' => $bio
+                                             ));
+    ?>
+
