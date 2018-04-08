214b1b56e2da03c14a416a0333ba3d2137525f0e
Logan Kerr <lkerr1998@gmail.com>
2018-04-08 06:11:28 -0400

    started working backend of stream

--- stream.php
+++ stream.php
@@ -20,10 +20,20 @@
     
     $admin = check_if_user_is_admin($_SESSION['user_id']);
     
+    $stmt = $conn->prepare("SELECT `users`.`firstName`, `users`.`lastName`, `goal_assoc`.`timestamp`, `goals`.`goalName`, `goals`.`goalText`, `profile_pictures`.`path` FROM `goal_assoc` INNER JOIN `goals` ON `goal_assoc`.`goal_id`=`goals`.`id` INNER JOIN `users` ON `goal_assoc`.`user_id`=`users`.`id` INNER JOIN `profile_pictures` ON `users`.`profile_picture_id`=`profile_pictures`.`id` ORDER BY `goal_assoc`.`timestamp` ASC");
+    $stmt->execute();
+    $res = $stmt->get_result();
+    $i = 0;
+    
+    while ($row = $res->fetch_assoc())
+    {
+        $rows[$i] = $row;
+        $i++;
+    }
+    
     echo $twig->render('stream.html', array(
                                              'nav' => array('page' => $_SERVER['PHP_SELF'], 'admin' => $admin),
-                                             'rows' => $rows,
-                                             'bio' => $bio
+                                             'rows' => $rows
                                              ));
     ?>
 
