7381e083aae76ba35ab2f191183f427c0c4602b5
Logan Kerr <lkerr1998@gmail.com>
2018-04-07 23:10:48 -0400

    Users can now cancel following users

--- removeFollowing.php
+++ removeFollowing.php
@@ -21,16 +21,34 @@
         $error = array();
         // get data
         
+        foreach ($_POST as $key => $value)
+        {
+            // for each user this user is following
+            if (substr($key, 0, 20) == "usersYouAreFollowing")
+            {
+                // if user wants to cancel following
+                if ($value == "cancel")
+                {
+                    $following_id = filter_var(substr($key, 20), FILTER_SANITIZE_NUMBER_INT);
+                    $stmt = $conn->prepare("DELETE FROM `follower_assoc` WHERE `user_id`=? AND `following_id`=?");
+                    $stmt->bind_param("ii", $user_id, $following_id);
+                    $stmt->execute();
+                }
+            }
+        }
+        
     } //ends post request
     
+    header("Location: followers.php");
+    
+    /*
     $loader = new Twig_Loader_Filesystem('resources/views');
     $twig = new Twig_Environment($loader);
     
     $admin = check_if_user_is_admin($_SESSION['user_id']);
     
-    echo $twig->render('profile.html', array(
-                                             'nav' => array('page' => $_SERVER['PHP_SELF'], 'admin' => $admin),
-                                             'rows' => $rows,
-                                             'bio' => $bio
+    echo $twig->render('followers.html', array(
+                                             'nav' => array('page' => $_SERVER['PHP_SELF'], 'admin' => $admin)
                                              ));
+     */
     ?>
