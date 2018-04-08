184e856f2b965042ff2808578d95c34bd16cc166
Derrick Hamilton <derrickhamilton212@gmail.com>
2018-04-07 20:02:39 -0400

    worked on index.html

--- login.php
+++ login.php
@@ -55,11 +55,12 @@
             exit();
         }
     }
-    
+
+    header('Location: index.php');
     $loader = new Twig_Loader_Filesystem('resources/views');
     $twig = new Twig_Environment($loader);
     
-    echo $twig->render('login.html', array(
+    echo $twig->render('index.html', array(
                                            'error' => $error,
                                            'email' => $email
                        ));
